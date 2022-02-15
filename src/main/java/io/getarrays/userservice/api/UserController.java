600aab003af4d1023be1c0d435f2bed0c579e7bc
joseluisgomes <joslus.luis@gmail.com>
2022-02-15 18:30:42 +0000

    RestController class updated

--- src/main/java/io/getarrays/userservice/api/UserController.java
+++ src/main/java/io/getarrays/userservice/api/UserController.java
@@ -1,29 +1,41 @@
 package io.getarrays.userservice.api;
 
+import io.getarrays.userservice.domain.Role;
 import io.getarrays.userservice.domain.User;
 import io.getarrays.userservice.service.UserService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("api/users")
+@RequestMapping("api")
 public class UserController {
     private final UserService userService;
 
-    @GetMapping
-    public ResponseEntity<List<User>> getUsers() {
-        return ResponseEntity.ok().body(userService.getUsers());
+    @GetMapping("/users")
+    public List<User> getUsers() {
+        return userService.getUsers();
     }
 
-    @GetMapping(path = "{username}")
+    @GetMapping("user/{username}")
     public User getUser(@PathVariable("username") String username) {
         return userService.getUser(username);
+    }
+
+    @PostMapping("save/user")
+    public User saveUser(@RequestBody User user) {
+        return userService.saveUser(user);
+    }
+
+    @PostMapping("save/role")
+    public Role saveRole(@RequestBody Role role) {
+        return userService.saveRole(role);
+    }
+
+    @PutMapping("update/user")
+    public void addRoleToUser(String username, String roleName) {
+        userService.addRoleToUser(username, roleName);
     }
 }
